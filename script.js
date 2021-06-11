0a0e639461c067fd4de1ba4c070ca7ddbb662818
Anya <annagapyuk@gmail.com>
2021-06-11 13:07:00 -0400

    project completed

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+// Promt the user to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error here
+    alert("check back later");
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable the button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+
+// on load
+selectMediaStream();
